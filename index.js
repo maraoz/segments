6a541c17d921a49015c63c8c04a428beccb9a5cd
Manuel Araoz <manuelaraoz@gmail.com>
2015-05-17 19:23:44 -0300

    update search algorithm to be deterministic

--- index.js
+++ index.js
@@ -10,14 +10,16 @@ var $ = bitcore.util.preconditions;
 var main = 'mzcc1oA5Aw4EfBvveKm8NEmZ93U9VFu3x3';
 var pk = 'cN76gKEk9w6rALN7gqEc9PNshgSQUXr4nTHybHZAibo1meewp2uE';
 var desiredAmount = Unit.fromBTC(0.01).toSatoshis();
-var MAX_SPEND = Unit.fromBTC(0.001).toSatoshis();
+var MAX_SPEND = Unit.fromBTC(0.01).toSatoshis();
 var fee = Unit.fromBTC(0.001).toSatoshis();
 
 var network = 'testnet';
 var insight = new explorers.Insight(network);
-var difficulty = Math.pow(2, 240);
+var difficulty = Math.pow(2, 245);
 
 console.log('difficulty', difficulty);
+console.log('search space size', MAX_SPEND);
+console.log('address', main);
 
 var transact = function() {
 
@@ -29,16 +31,18 @@ var transact = function() {
 
     var bestHash = null;
     var mining = true;
+    var delta = 0;
     while (mining) {
-
-      var satoshis = desiredAmount + Math.floor(Math.random() * MAX_SPEND);
+      delta += 1;
+      $.checkState(delta < MAX_SPEND, 'Couldnt find satisfying tx in all search space.');
+      var satoshis = desiredAmount + delta;
 
       var tx = new Transaction()
         .change(main)
         .from(utxos)
         .to(main, satoshis)
-        .fee(fee)
-        .sign(pk);
+        .fee(fee);
+      tx.sign(pk);
 
       var hashValue = parseInt(tx.hash, 16);
       if (bestHash === null || hashValue < bestHash) {
@@ -46,6 +50,8 @@ var transact = function() {
         console.log(tx.id, bestHash);
         if (bestHash < difficulty) {
           console.log('will broadcast', tx.id);
+          console.log(tx);
+          console.log(tx.serialize());
           insight.broadcast(tx, function(err, txid) {
             if (err) {
               throw err;
